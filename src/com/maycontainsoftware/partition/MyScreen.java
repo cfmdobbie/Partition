c274bf8f7ffbb0b904c7a4000441796eea6f48b2
cfmdobbie <cfmdobbie@gmail.com>
2014-11-25 17:59:04 +0000

    Comments

--- src/com/maycontainsoftware/partition/MyScreen.java
+++ src/com/maycontainsoftware/partition/MyScreen.java
@@ -5,19 +5,36 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
+/**
+ * A Screen instance.
+ * 
+ * @author Charlie
+ */
 public class MyScreen extends ScreenAdapter {
 
+	/** Tag for logging purposes. */
 	public static final String TAG = MyScreen.class.getName();
 
+	/** Reference to the Game instance. */
 	private final PartitionGame game;
+
+	/** Reference to the current game state. */
 	private GameState state;
 
+	/** Player colors, arranged from best to worst. */
 	private static final Color[] PLAYER_COLORS = { Color.RED, Color.BLUE, Color.GREEN, Color.CYAN, Color.MAGENTA,
 			Color.ORANGE, Color.PINK, Color.YELLOW, };
 
+	// TEMP: Board configurations used for testing
 	public static final String BOARD_1 = "....\n.0#.\n.#1.\n....";
 	public static final String BOARD_2 = "#.....#\n..0.1..\n.7#.#2.\n.......\n.6#.#3.\n..5.4..\n#.....#";
 
+	/**
+	 * Constructor
+	 * 
+	 * @param game
+	 *            The Game instance.
+	 */
 	public MyScreen(final PartitionGame game) {
 		this.game = game;
 		this.state = GameState.newGameState(BOARD_2);
@@ -41,13 +58,18 @@ public class MyScreen extends ScreenAdapter {
 		}
 		game.shapeRenderer.end();
 
+		// Determine board size
 		final int columns = this.state.tileEnabled.length;
 		final int rows = this.state.tileEnabled[0].length;
 
+		// Determine tile size
 		final float tileWidth = w / columns;
 		final float tileHeight = h / rows;
 
+		// Start drawing
 		game.shapeRenderer.begin(ShapeType.Filled);
+
+		// Draw tiles
 		game.shapeRenderer.setColor(Color.WHITE);
 		for (int c = 0; c < columns; c++) {
 			for (int r = 0; r < rows; r++) {
@@ -59,6 +81,8 @@ public class MyScreen extends ScreenAdapter {
 				}
 			}
 		}
+
+		// Draw players
 		for (int p = 0; p < GameState.getNumberOfPlayers(state); p++) {
 			game.shapeRenderer.setColor(PLAYER_COLORS[p]);
 			final byte[] coords = GameState.getPlayerCoords(state, p);
@@ -67,6 +91,8 @@ public class MyScreen extends ScreenAdapter {
 			game.shapeRenderer
 					.ellipse(x + tileWidth * 0.2f, y + tileHeight * 0.2f, tileWidth * 0.6f, tileHeight * 0.6f);
 		}
+
+		// Stop drawing
 		game.shapeRenderer.end();
 	}
 
--- src/com/maycontainsoftware/partition/PartitionGame.java
+++ src/com/maycontainsoftware/partition/PartitionGame.java
@@ -7,13 +7,26 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+/**
+ * The main Game instance.
+ * 
+ * @author Charlie
+ */
 public class PartitionGame extends Game {
 
+	/** Tag for logging purposes. */
 	public static final String TAG = PartitionGame.class.getName();
+
+	/** App-global debug flag, for controlling debug output etc. */
 	public static final boolean DEBUG = true;
 
-	OrthographicCamera camera;
+	/** The OpenGL camera used by the app. There is only one camera and it is managed entirely by the Game instance. */
+	private OrthographicCamera camera;
+
+	/** The raster graphics renderer. There is only one SpriteBatch and it is held by the Game instance. */
 	SpriteBatch batch;
+
+	/** The primitive renderer. There is only one ShapeRenderer and it is held by the Game instance. */
 	ShapeRenderer shapeRenderer;
 
 	@Override
@@ -21,15 +34,20 @@ public class PartitionGame extends Game {
 		final float w = Gdx.graphics.getWidth();
 		final float h = Gdx.graphics.getHeight();
 
+		// Create camera of same dimensions as screen buffer
 		camera = new OrthographicCamera(w, h);
+
+		// Create rendering objects
 		batch = new SpriteBatch();
 		shapeRenderer = new ShapeRenderer();
 
+		// TEMP: For now, just start with MyScreen
 		setScreen(new MyScreen(this));
 	}
 
 	@Override
 	public void resize(int width, int height) {
+		// Update camera wrt changed screen dimensions
 		camera.setToOrtho(false, width, height);
 	}
 
@@ -42,7 +60,7 @@ public class PartitionGame extends Game {
 	@Override
 	public void render() {
 
-		// Update camera
+		// Update camera projection
 		camera.update();
 
 		// Update renderer projections
