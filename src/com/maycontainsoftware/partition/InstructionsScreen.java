d9f768417c4e33544ffa33c855a8919702f243ef
cfmdobbie <cfmdobbie@gmail.com>
2014-12-02 11:30:22 +0000

    Progress on basic instructions screen layout

--- src/com/maycontainsoftware/partition/InstructionsScreen.java
+++ src/com/maycontainsoftware/partition/InstructionsScreen.java
@@ -1,19 +1,46 @@
 package com.maycontainsoftware.partition;
 
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
 
 public class InstructionsScreen extends BaseScreen {
 
 	public InstructionsScreen(final PartitionGame game) {
 		super(game);
 
-		@SuppressWarnings("unused")
 		final TextureAtlas atlas = game.manager.get("atlas.atlas", TextureAtlas.class);
 
-		// Plan:
-		// Back button to go to top menu
-		// Sound button to toggle sound on/off
-		// Scrolling pane containing VverticalGroup
-		// Assets/controls to represent instruction information
+		root.setBackground(new TiledDrawable(atlas.findRegion("background")));
+		root.defaults().pad(5.0f);
+
+		// Top menu bar
+		root.row();
+		final Table topBar = new Table();
+		root.add(topBar).fill();
+
+		// Widgets in top menu bar
+		topBar.row();
+		topBar.add(); // TODO: back button
+		topBar.add().expandX();
+		topBar.add(new Button(new TextureRegionDrawable(atlas.findRegion("sound_on"))));
+
+		// Divider
+		root.row().height(2.0f);
+		root.add(new Image(atlas.findRegion("white1x1"))).fill();
+
+		// Scrollable group to hold instructions
+		root.row();
+		final Group instructions = new VerticalGroup();
+		final ScrollPane scroll = new ScrollPane(instructions);
+		root.add(scroll).expand().fill();
+
+		// TODO: Instructions
 	}
 }
