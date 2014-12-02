0c53ff834ec92ef958fdcaebc0ee3c2168f3f80b
cfmdobbie <cfmdobbie@gmail.com>
2014-12-02 15:13:52 +0000

    Remove unwanted file from asset manager

--- src/com/maycontainsoftware/partition/LoadingScreen.java
+++ src/com/maycontainsoftware/partition/LoadingScreen.java
@@ -69,7 +69,6 @@ public class LoadingScreen extends BaseScreen {
 	private void enqueueTextures() {
 		// TODO: Enqueue Textures
 		game.manager.load("yellow.png", Texture.class);
-		game.manager.load("Background.png", Texture.class);
 	}
 
 	/** Enqueue all Sound assets required by the app. */
