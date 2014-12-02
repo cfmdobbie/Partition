40ea37f2107164ec6591f88395084b66b898befa
cfmdobbie <cfmdobbie@gmail.com>
2014-12-02 13:20:31 +0000

    Stub functionality for instructions screen

--- src/com/maycontainsoftware/partition/InstructionsScreen.java
+++ src/com/maycontainsoftware/partition/InstructionsScreen.java
@@ -1,13 +1,16 @@
 package com.maycontainsoftware.partition;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.VerticalGroup;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
@@ -47,12 +50,42 @@ public class InstructionsScreen extends BaseScreen {
 		root.row().height(2.0f);
 		root.add(new Image(atlas.findRegion("white1x1"))).fill();
 
-		// Scrollable group to hold instructions
-		root.row();
-		final Group instructions = new VerticalGroup();
-		final ScrollPane scroll = new ScrollPane(instructions);
-		root.add(scroll).expand().fill();
+		// Scrollable table to hold instructions
+		final Table instructions = new Table();
+		instructions.defaults().pad(10.0f);
+		// instructions.debug();
 
-		// TODO: Instructions
+		// Add table to stage, wrapped in a scrolling pane
+		root.row();
+		root.add(new ScrollPane(instructions)).expand().fill();
+
+		// Fonts
+		final BitmapFont bodyFont = new BitmapFont(Gdx.files.internal("segoeuiblack16.fnt"));
+		final BitmapFont titleFont = new BitmapFont(Gdx.files.internal("segoeuiblack32.fnt"));
+		// Styles
+		final LabelStyle bodyStyle = new Label.LabelStyle(bodyFont, Color.WHITE);
+		final LabelStyle titleStyle = new Label.LabelStyle(titleFont, Color.BLUE);
+
+		// Instructions
+		// TODO: Proper instructions!
+		instructions.row();
+		instructions.add(new Label("Title Text", titleStyle));
+
+		final String[] lines = {
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+				"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+				"Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." };
+		for (String line : lines) {
+			final Label label = new Label(line, bodyStyle);
+			label.setWrap(true);
+			instructions.row();
+			instructions.add(label).fillX().expandX();
+		}
+
+		instructions.row();
+		instructions.add(new Image(atlas.findRegion("Tile")));
+		instructions.row();
+		instructions.add(new Image(atlas.findRegion("RedPlayer")));
 	}
 }
