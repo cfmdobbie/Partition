e1d697ecbd2bfb50206cde7a4407bde325f03d87
cfmdobbie <cfmdobbie@gmail.com>
2015-03-23 13:23:38 +0000

    Typo fix

--- src/com/maycontainsoftware/partition/MainMenuScreen.java
+++ src/com/maycontainsoftware/partition/MainMenuScreen.java
@@ -24,7 +24,7 @@ public class MainMenuScreen extends CScreen<PartitionGame> {
 	private final ScreenTransition screenTransition;
 
 	/**
-	 * Construct a new MainMenuScree.
+	 * Construct a new MainMenuScreen.
 	 * 
 	 * @param game
 	 *            The PartitionGame instance.
