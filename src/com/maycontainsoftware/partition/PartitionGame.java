bbd25f51b5c0b82d32dd9da7b0d5c086f588b0e4
cfmdobbie <cfmdobbie@gmail.com>
2015-04-18 13:21:27 +0100

    Additional demonstration modes for use on instructions screen

--- src/com/maycontainsoftware/partition/PartitionGame.java
+++ src/com/maycontainsoftware/partition/PartitionGame.java
@@ -29,7 +29,10 @@ public class PartitionGame extends CGame {
 		WALL(".......\n.......\n.......\n...#...\n..0#1..\n...#...\n.......\n.......\n......."),
 		// TODO: HOLES *may* be a bust, need to play-test to determine whether it's worth keeping
 		HOLES("...1...\n.#.#.#.\n.......\n.#.#.#.\n.......\n.#.#.#.\n.......\n.#.#.#.\n...0..."),
-		MAIN_MENU_DEMO("..0..");
+		MAIN_MENU_DEMO("..0.."),
+		LOSE_DEMO(".01."),
+		DRAW_DEMO(".01"),
+		STALEMATE_DEMO("0.1");
 
 		public final String boardSpec;
 
