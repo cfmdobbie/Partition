5a146680350ebb13d9d8feba2c76c5ed35d85eae
cfmdobbie <cfmdobbie@gmail.com>
2015-05-04 17:55:00 +0100

    Detailed player configuration

--- src/com/maycontainsoftware/partition/PartitionGame.java
+++ src/com/maycontainsoftware/partition/PartitionGame.java
@@ -16,8 +16,39 @@ public class PartitionGame extends CGame {
 
 	/** Enumeration of player configurations. */
 	public static enum PlayerConfiguration {
-		TWO_PLAYER,
-		ONE_PLAYER_VS_COMPUTER,
+		ONE_PLAYER(false),
+		TWO_PLAYER(false, false),
+		ONE_PLAYER_VS_COMPUTER(false, true),
+		TWO_COMPUTERS(true, true);
+
+		/** Flags to show which players are computer-controlled. */
+		private final boolean[] isComputerPlayer;
+
+		/**
+		 * Construct a new PlayerConfiguration.
+		 * 
+		 * @param isComputerPlayer
+		 *            Array/varargs of flags to determine which players are computer-controlled.
+		 */
+		private PlayerConfiguration(boolean... isComputerPlayer) {
+			this.isComputerPlayer = isComputerPlayer;
+		}
+
+		/**
+		 * @return The number of players.
+		 */
+		public final int getNumberOfPlayers() {
+			return isComputerPlayer.length;
+		}
+
+		/**
+		 * @param playerNumber
+		 *            The player number.
+		 * @return True if the player is computer-controlled, false otherwise.
+		 */
+		public final boolean isComputerPlayer(final int playerNumber) {
+			return isComputerPlayer[playerNumber];
+		}
 	}
 
 	/** Enumeration of implemented boards. */
